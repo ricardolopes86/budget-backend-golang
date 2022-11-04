f95fd6b5395d11cba84dc77d7122e2f4c889142b
ricardinho <ricardolopes18@yahoo.com.br>
2022-11-04 21:39:55 +0100

    Criada a rota para aceitar o update de Entrada

--- main.go
+++ main.go
@@ -16,6 +16,6 @@ func main() {
 	router.GET("/entradas", controllers.FindEntradas)
 	router.GET("/entradas/:id", controllers.FindEntrada)
 	router.DELETE("/entradas/:id", controllers.DeleteEntrada)
-
+	router.PATCH("/entradas/:id", controllers.UpdateEntrada)
 	router.Run("localhost:9000")
 }
